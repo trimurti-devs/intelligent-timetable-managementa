59e76e4e771cc279e9748de1aaa381ae23761f59
Abhishek Ghosh <abhishekghoshhetc712102@gmail.com>
2025-10-15 00:15:53 +0530

    Delete temp_check_faculty.py

--- temp_check_faculty.py
+++
@@ -1,6 +0,0 @@
-from app import app, db, Faculty
-
-with app.app_context():
-    faculties = Faculty.query.all()
-    for f in faculties:
-        print(f"Faculty: {f.name}, Availability: {f.availability}")
