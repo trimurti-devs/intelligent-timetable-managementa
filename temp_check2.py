44bfc24cd7e72e51d02947781ca9bf79d65d1901
Abhishek Ghosh <abhishekghoshhetc712102@gmail.com>
2025-10-15 00:15:32 +0530

    Delete temp_check2.py

--- temp_check2.py
+++
@@ -1,6 +0,0 @@
-from app import app, db, Course, Faculty
-with app.app_context():
-    courses = Course.query.all()
-    for c in courses:
-        faculty_name = c.faculty.name if c.faculty else 'None'
-        print(f"{c.name}: {faculty_name}")
